62d21c3a22ca6cde8e24661c15a9892879a47d6c
Fernando <fernandoperezverdejo@gmail.com>
2023-03-27 12:51:13 -0300

    login parte 5

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ import { escribirArchivo, leerArchivo ,alertaSI } from "../utils/handlers.js";
 import { send } from "process";
 import { time, timeEnd } from "console";
 import { Script } from "vm";
+import connection from "../server.js";
 const router = Router();
 
 //===================GET===================//
@@ -20,31 +21,60 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.post('/app', async (req, res) => {
-    let user = req.body.user;
-    let pass = req.body.pass;
-    const datausuarios = await leerArchivo('./data/login.json');
-    const datapacientes = await leerArchivo('./data/pacientes.json');
+// router.post('/app', async (req, res) => {
+//     let user = req.body.user;
+//     let pass = req.body.pass;
+//     const datausuarios = await leerArchivo('./data/login.json');
+//     const datapacientes = await leerArchivo('./data/pacientes.json');
 
-    let arrUsuarios = datausuarios.users;
-    console.log(arrUsuarios);
-    let arrPacientes = datapacientes.pacientes;
-    console.log(arrPacientes);
-    console.log(user);
-    console.log(pass);
-    let llave = false;
-    arrUsuarios.forEach(element => {
-        if (user == element.nombre && pass == element.contrasena) {
-            console.log(element.nombre);
-            console.log(element.contrasena);
-            llave = true;
-        }
-    });
-    if (llave == true) {
-            res.send("<Script>alert('Login exitoso')</Script>" .render("home"))
-    }
-    else {
-        res.render("login2")
-    }       
+//     let arrUsuarios = datausuarios.users;
+//     console.log(arrUsuarios);
+//     let arrPacientes = datapacientes.pacientes;
+//     console.log(arrPacientes);
+//     console.log(user);
+//     console.log(pass);
+//     let llave = false;
+//     arrUsuarios.forEach(element => {
+//         if (user == element.nombre && pass == element.contrasena) {
+//             console.log(element.nombre);
+//             console.log(element.contrasena);
+//             llave = true;
+//         }
+//     });
+//     if (llave == true) {
+//             res.render("login1")
+//     }
+//     else {
+//         res.render("login2")
+//     }       
+// });
+router.post('/app', function(request, response) {
+	// Capture the input fields
+	let user = request.body.username;
+	let pass = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (user && pass) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [user, pass], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = user;
+				// Redirect to home page
+				response.render('login1');
+			} else {
+				response.render('login2');
+			}
+
+			//response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+
 });
 export default router;
--- server.js
+++ server.js
@@ -1,29 +1,46 @@
 import express from "express";
-import {dirname, join} from "path";
-import {fileURLToPath} from "url";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 import indexRoutes from "./routes/routes.js";
 import * as helpers from "./utils/helpers/hbs.js";
 import hbs from "hbs";
-
 import bodyParser from 'body-parser';
+import session from "express-session";
+import mysql from 'mysql';
 
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Yuseiwheel2021',
+  database: 'dbportafolio'
+})
 const app = express();
+
+app.use(session({
+  secret:'secret',
+  resave:true,
+  saveUninitialized:true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
-})); 
+}));
 
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-hbs.registerPartials(join(__dirname,"/views/partials"));
+hbs.registerPartials(join(__dirname, "/views/partials"));
 app.use(indexRoutes);
 app.set('view engine', 'hbs');
-app.set('views' , './views');
+app.set('views', './views');
 app.use(express.static("public"));
 
 
 
 app.listen(3000);
+export default connection;
